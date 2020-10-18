b24afc447aca15488ab7291cfe40027a8d004487
Olavo <olavo.santos@skipthedishes.ca>
2020-10-18 16:03:37 -0500

    added gql client type

--- src/app/api/types.ts
+++ src/app/api/types.ts
@@ -2,11 +2,10 @@ import { AxiosRequestConfig as RESTOptions } from 'axios';
 import { GraphQLClient as GraphQLRequest } from 'graphql-request';
 import { RequestInit as GraphQLOptions } from 'graphql-request/dist/types.dom';
 
+export type GraphQLClient = (endpoint: string) => GraphQLRequest;
 export interface ApiConfig {
   clients: {
     rest: RESTOptions;
     graphql: GraphQLOptions;
   };
 }
-
-export type GraphQLClient = (endpoint: string) => GraphQLRequest;
